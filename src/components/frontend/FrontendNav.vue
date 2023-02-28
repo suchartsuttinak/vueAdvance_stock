c270d6349314aecea71d39ea47e7b6fdc2d2fc8c
suchart <suchartsuttinak@gmail.com>
2023-02-28 13:43:36 +0700

    menu

--- src/components/frontend/FrontendNav.vue
+++ src/components/frontend/FrontendNav.vue
@@ -6,20 +6,20 @@
         <div class="flex items-center">
             <div class="hidden space-x-6 font-medium md:block">
             <router-link to="/" class="transition-colors duration-200 hover:text-emerald-700">Home</router-link>
-            <router-link to="/Features" class="transition-colors duration-200 hover:text-emerald-700">Features</router-link>
-            <router-link to="/Servicies" class="transition-colors duration-200 hover:text-emerald-700">Servicies</router-link>
-            <router-link to="/Testimail" class="transition-colors duration-200 hover:text-emerald-700">Testimail</router-link>
-            <router-link to="/Contacts" class="transition-colors duration-200 hover:text-emerald-700">Contacts</router-link>
+            <router-link to="/features" class="transition-colors duration-200 hover:text-emerald-700">Features</router-link>
+            <router-link to="/servicies" class="transition-colors duration-200 hover:text-emerald-700">Servicies</router-link>
+            <router-link to="/testimonials" class="transition-colors duration-200 hover:text-emerald-700">Testimonials</router-link>
+            <router-link to="/contacts" class="transition-colors duration-200 hover:text-emerald-700">Contacts</router-link>
             <router-link to="/getstarted">
                 <button class="bg-emerald-600 hover:bg-emerald-700 transition-colors duration-300 py-2.5 px-5 rounded-lg text-white font-semibold">Get Started</button>
             </router-link>
             </div>
             <div class="md:hidden">
-                <a href="#0">
+                <router-link to="#0">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                       </svg>
-                </a>
+                    </router-link>
             </div>
         </div>
     </header><!-- End Header -->
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 import FrontendLayoutVue from '../layouts/FrontendLayout.vue'
+import ContactViewVue from '../views/frontend/ContactView.vue'
+import FeaturesViewVue from '../views/frontend/FeaturesView.vue'
+import GetStartedViewVue from '../views/frontend/GetStartedView.vue'
 import HomeViewVue from '../views/frontend/HomeView.vue'
+import ServiceViewVue from '../views/frontend/ServiceView.vue'
+import TestimonialsViewVue from '../views/frontend/TestimonialsView.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -18,7 +23,33 @@ const routes: Array<RouteRecordRaw> = [
                 path: '',
                 name: 'Home',
                 component: HomeViewVue
-            }
+            },
+            {
+                path: '/features',
+                name: 'Features',
+                component: FeaturesViewVue
+            },
+            {
+                path: '/servicies',
+                name: 'Service',
+                component: ServiceViewVue
+            },
+            {
+                path: '/testimonials',
+                name: 'Testimonials',
+                component: TestimonialsViewVue
+            },
+            {
+                path: '/contacts',
+                name: 'Contacts',
+                component: ContactViewVue
+            },
+            {
+                path: '/getstarted',
+                name: 'Getstart',
+                component: GetStartedViewVue
+            },
+            
         ]
     },
 ]
